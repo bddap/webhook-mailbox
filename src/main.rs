5e66c64db921ae18f13c190669013dd447b1dfa1
Andrew Dirksen <andrew@dirksen.com>
2022-03-18 17:17:14 -0700

    add ci

--- src/main.rs
+++ src/main.rs
@@ -18,5 +18,7 @@ mod service;
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    endpoints::rocket_build(service::Db::default()).launch().await
+    endpoints::rocket_build(service::Db::default())
+        .launch()
+        .await
 }
